2c5923d49acb6e4c29cd993a817b270986fb9050
Diogo Amaral <dlamaral12@gmail.com>
2022-06-20 22:08:00 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Diogo Amaral on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
